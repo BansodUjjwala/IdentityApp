51bf12e6ccd669b856d85e43032bd2040e8c4a69
Ujjwala Bansod <ujjwalabansod21@gmail.com>
2025-10-15 12:55:42 +0530

    02,Api project setup,AppNetCore Identity added

---
+++ Api/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+namespace Api.Models
+{
+    public class User:IdentityUser
+    {
+        [Required]
+        public string Firstname { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+        public DateTime DateCreated  { get; set; } = DateTime.UtcNow;
+
+       
+    }
+}
